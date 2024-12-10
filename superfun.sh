88ac43b3a9cb3a84eb9380461a892e7104045c66
Aarav R <127631010+AaravR10@users.noreply.github.com>
2024-12-10 09:52:27 -0800

    Update superfun.sh

--- superfun.sh
+++ superfun.sh
@@ -5,7 +5,7 @@ if [ "$EUID" -ne 0 ]; then
 fi
 random_number=$((1 + $RANDOM % 6))
 echo "Linux roulette..."
-read -p "Choose a number between 1 and 6" choice
+read -p "Choose a number between 1 and 6: " choice
 if [[ $choice == $random_number ]]; then
   echo "You win!!!"
 else
